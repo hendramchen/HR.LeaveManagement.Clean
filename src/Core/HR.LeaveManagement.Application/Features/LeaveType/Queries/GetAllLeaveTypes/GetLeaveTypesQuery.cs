259386856c661fa7a25eecd9240601426d8a7a1e
Hendra Putu <mchen.putu@gmail.com>
2025-03-09 17:14:28 +0800

    add implementation of CQRS

--- src/Core/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQuery.cs
+++ src/Core/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQuery.cs
@@ -2,7 +2,8 @@ using MediatR;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
 
-public class GetLeaveTypesQuery : IRequest<List<LeaveTypeDto>>
-{
-    
-}
+// public class GetLeaveTypesQuery : IRequest<List<LeaveTypeDto>>
+// {
+// }
+
+public record GetLeaveTypesQuery : IRequest<List<LeaveTypeDto>>;
